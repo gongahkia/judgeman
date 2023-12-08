da36a367cda6a5ee15e6cf7076c4a7ccff377174
gongahkia <angryapplegravy@gmail.com>
2023-12-08 20:49:25 +0800

    nice job

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 // FUA
-    // add code below the table parsing to examine the txt.body after the table we just iterated over of class="txt-body"
-    // check whether other possible elit file formats that dont fit this specified mold
-    // add further code under body content to check whether an existing field has been filled up under a diff recognised name and dont edit if so (eg. judgement and conclusion)
-    // format final output text to be as easy to read as possible using pure html and css and ask gpt to help me format this
-    // allow the extension to run when the button is clicked
-    // optionally allowing displaying of components via folding so that no important information is omitted
+    // 2 implement
+        // specific body content (main.js)
+            // change and trim existing code 
+            // add display of all headers of class:"Judg-Heading-1" via folding so that no important information is omitted
+        // frontend (main.js)
+            // format final output text to be as easy to read as possible using pure html and css and ask gpt to help me format this
+            // responsive HTML and CSS
+            // might have to add another html file here that is later generated dynamically if rendering doesn't work out, or change the core page content fundamentally 
+            // allow toggling of page content
+        // extension specific (manifest.json)
+            // allow the extension to run when the button is clicked
+            // clean up extension description as well, make manifest.json code cleaner
+    // playtest 
+        // check other possible elit file formats that dont fit this specified mold
+        // check other browsers that might not support this extension
 
 // -------------------- THIS CODE GOES IN CHRONOLOGICAL ORDER --------------------
 
-// ---------- HEADER ----------
+// ---------- UNIVERSAL HEADER ---------- DONE!!!
 
 var page = {
     caseTitle: "",
@@ -18,24 +27,23 @@ var page = {
     caseCoram: "",
     caseCounsel: "",
     caseParties: "",
-    caseLegalIssues: "",
-    caseIntro: "",
-    caseFacts: "",
-    caseJudgmentConclusion: ""
+    caseLegalIssues: [],
+    caseBody: {}
 };
 
 var count = 0;
 
+// case title
 var a = document.querySelector(".caseTitle");
-// a.style.backgroundColor = "yellow";
+a.style.backgroundColor = "green";
 page.caseTitle = a.textContent.trim();
 
+// case summary data
 var infoTable = document.querySelector("#info-table");
-// infoTable.style.backgroundColor = "yellow";
+infoTable.style.backgroundColor = "yellow";
 for (var i=0; i < infoTable.rows.length; i++) {
     var cells = infoTable.rows[i].cells;
     for (var q=0; q< cells.length; q++) {
-        // console.log(`i is ${i} and q is ${q} and ${cells[q].innerText}`);
         if (i === 0 && q === 2) {
             page.caseNumber = cells[q].innerText;
         } else if (i === 1 && q === 2) {
@@ -54,16 +62,22 @@ for (var i=0; i < infoTable.rows.length; i++) {
     }
 }
 
-/* 
-alert(`This case is called ${page.caseTitle}`);
-alert(`This case date is ${page.caseDate}`);
-alert(`This case tribunal and courts is ${page.caseTribunalCourt}`);
-alert(`This case coram is ${page.caseCoram}`);
-alert(`This case counsel is ${page.caseCounsel}`);
-alert(`The case parties are ${page.caseParties}`);
-*/
+// case legal issues
+var legalIssues = document.querySelector("div.txt-body");
+legalIssues.style.backgroundColor = "magenta";
+if (legalIssues) {
+    legalIssues.childNodes.forEach((childNode) => {
+        if (childNode.nodeType === 1 && childNode.textContent != "") {
+            page.caseLegalIssues.push(childNode.textContent.trim());
+        }
+    });
+} else {
+    console.log(`Edge case ${String(count).padStart(3,'0')} hit. Report any issues to @gongahkia.`);
+}
 
-// ---------- BODY CONTENT -----------
+// alert(`Case title: ${page.caseTitle}\nCase date: ${page.caseDate}\nCase tribunal / Courts: ${page.caseTribunalCourt}\nCase coram: ${page.caseCoram}\nCase counsel: ${page.caseCounsel}\nCase parties: ${page.caseParties}\nCase legal issues: ${page.caseLegalIssues}`);
+
+// ---------- SPECIFIC BODY CONTENT -----------
 
 var imp = document.querySelectorAll(".Judg-Heading-1");
 imp.forEach(function (paragraph) {
