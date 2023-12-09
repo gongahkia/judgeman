c571c50551c74e742f96d30bfb50c16cd8fdae3c
gongahkia <angryapplegravy@gmail.com>
2023-12-09 21:03:21 +0800

    added soem code

--- main.js
+++ main.js
@@ -121,13 +121,99 @@ page.caseBody[secName] = buffer;
 // ---------- POP-UP and CHANGE HTML CONTENT ----------
 
 function sanitise(b) {
-    fin = "";
-    return fin;
+    return b.replace(/^\s*\d+\s*/gm, "");
+}
+
+function addSection(sum, deets) {
+    var mainEl = document.getElementById("dynamic-body");
+    var deetEl = document.createElement("details");
+    var sumEl = document.createElement("summary");
+    var deetContentEl = document.createElement("p");
+    sumEl.textContent = sum;
+    deetContentEl.innerHTML = deets;
+    deetEl.appendChild(sumEl);
+    deetEl.appendChild(deetContentEl);
+    mainEl.appendChild(deetEl);
 }
 
 function simplifyContent(page) {
     var backup = document.body.innerHTML;
-    document.body.innerHTML = "";
+    var secNum = 0;
+    var styleContent = `
+    body {
+        font-family: 'Arial', sans-serif;
+        background-color: #f8f8f8;
+        color: #333;
+        margin: 0;
+        padding: 0;
+    }
+
+    header {
+        background-color: #333;
+        color: #fff;
+        text-align: center;
+        padding: 1em;
+    }
+
+    main {
+        max-width: 800px;
+        margin: 2em auto;
+        padding: 1em;
+        background-color: #fff;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+
+    details {
+        margin-bottom: 1em;
+    }
+
+    summary {
+        cursor: pointer;
+        font-weight: bold;
+        border-bottom: 1px solid #ccc;
+        padding-bottom: 0.5em;
+        margin-bottom: 0.5em;
+        outline: none;
+    }
+
+    details > p {
+        margin-top: 0;
+    }
+
+    .github-credit {
+        position: fixed;
+        bottom: 10px;
+        right: 10px;
+        font-size: 12px;
+        color: #555;
+    }`
+    ;
+
+    document.body.innerHTML = `
+    <header>
+        <h1 id="dynamic-header"></h1>
+    </header>
+
+    <main id="dynamic-body">
+    </main>
+
+    <div class="github-credit">
+        Designed and built by <a href="https://gongahkia.github.io/">Gabriel Ong</a> | <a href="https://github.com/gongahkia/cfp">Source</a>
+    </div>
+    `;
+    document.title = "CFP";
+
+    var styleEl = document.querySelector("style");
+    if (styleEl) {
+        styleEl.innerHTML = styleContent;
+    } else {
+        var newStyleEl = document.createElement("style");
+        newStyleEl.innerHTML = styleContent;
+        document.head.appendChild(newStyleEl);
+    }
+
+    // add code here to add each case info as additional choice
+
     caseTitle = page.caseTitle;
     caseNumber = page.caseNumber;
     caseDate = page.caseDate;
@@ -137,17 +223,22 @@ function simplifyContent(page) {
     caseParties = page.caseParties;
     caseLegalIssuesArray = page.caseLegalIssues;
     caseBodyArray = page.caseBody;
-    // document.body.innerText = "edit ass please thanks"; 
-    a = `${caseTitle}\n${caseNumber}\n${caseDate}\n${caseTribunalCourt}\n${caseCoram}\n${caseCounsel}\n${caseParties}\n${caseLegalIssuesArray}\n`;
 
-    // add a function to sanitise the string
+    var dynHead = document.getElementById("dynamic-header");
+    dynHead.textContent = caseTitle;
+
+    // document.body.innerText = "edit ass please thanks"; 
+    // document.body.innerText = `${caseTitle}\n${caseNumber}\n${caseDate}\n${caseTribunalCourt}\n${caseCoram}\n${caseCounsel}\n${caseParties}\n${caseLegalIssuesArray}\n\n\n`;
+
     for (el in caseBodyArray) {
+        buf = "";
         for (var q=0; q < caseBodyArray[el].length; q++) {
-            a = a.concat(el, "=>", q, "=>", caseBodyArray[el][q], "\n\n\n");
+            secNum += 1;
+            buf += `<p>${secNum}.&emsp;${sanitise(caseBodyArray[el][q])}</p>`;
         }
+        addSection(el, buf);
     }
 
-    document.body.innerText = a;
     return backup;
 }
 
