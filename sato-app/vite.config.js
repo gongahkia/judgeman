3e80dfb2c396f3a956860268745e302ed2304cd6
Gabriel Ong Zhe Mian <angryapplegravy@gmail.com>
2025-05-05 11:18:04 +0800

    feat(app): crash out!
    (cherry picked from commit f65f2e741a7a60ee8e2b5927d86fea877f5a8ed3)

--- sato-app/vite.config.js
+++ sato-app/vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
-    host: true, // or '0.0.0.0' for all interfaces
+    host: true, 
     port: 5173
   }
 })
